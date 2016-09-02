70f2ba903fb62971976d45c8dc361bb3085b096b
Varsity Software <github@varsitysoftware.com>
2016-09-02 00:26:42 -0400

    Create deleteFiles.m

---
+++ src/ios/deleteFiles.m
@@ -0,0 +1,34 @@
+//
+//  deleteFiles.m
+//
+//  Created by John Weaver on 08/30/16
+//
+//
+
+#import "deleteFiles.h"
+
+@implementation DeleteFiles 
+
+@synthesize callbackId;
+
+- (void)clearTempDirectory
+{
+    NSArray* tmpDirectory = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:NSTemporaryDirectory() error:NULL];
+    for (NSString *file in tmpDirectory) {
+		NSLog(@"REMOVING FILE: %@", file);    
+        [[NSFileManager defaultManager] removeItemAtPath:[NSString stringWithFormat:@"%@%@", NSTemporaryDirectory(), file] error:NULL];
+    }
+}
+
+- (void) delete:(CDVInvokedUrlCommand *)command {
+    
+    NSDictionary *options = [command.arguments objectAtIndex: 0];
+  
+    self.callbackId = command.callbackId;
+
+	  [self clearTempDirectory];
+	  
+	  CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+    [self.commandDelegate sendPluginResult:result callbackId:self.callbackId];
+    
+}
